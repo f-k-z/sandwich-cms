e012933c1c8ef2799d097860ebd6af45f0c829df
f-k-z@github.com <gilletfrancois@gmail.com>
2017-04-08 08:00:34 +1100

    True to set up Firebase as global

--- sandwich-cms/src/App.vue
+++ sandwich-cms/src/App.vue
@@ -2,13 +2,34 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <router-view></router-view>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Cover</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects">
+          <td><a v-bind:href="project.url">{{project.title}}</a></td>
+          <td>{{project.description}}</td>
+          <td><img v-bind:src="project.cover.url" /></td>
+          <td><span class="glyphicon glyphicon-trash" aria-hidden="true" v-on:click="removeProject(project)"></span></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import global from '@/global'
 
 export default {
   name: 'app',
+  firebase: {
+    projects: global.db.ref('projects')
+  },
   data () {
     return {
       title: 'Sandwich CMS'
--- sandwich-cms/src/global.js
+++ sandwich-cms/src/global.js
@@ -1,3 +1,17 @@
+import Firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcP7Mdrq2LkOshkwZWGGwz-T8yFfBihCI",
+    authDomain: "test-project-c9e5c.firebaseapp.com",
+    databaseURL: "https://test-project-c9e5c.firebaseio.com",
+    storageBucket: "test-project-c9e5c.appspot.com",
+    messagingSenderId: "70554115470"
+  };
+  
+const app = Firebase.initializeApp(firebaseConfig)
+const db = app.database()
+
 export default {
-	test: 'Variable Global !!'
+	test: 'Variable Global !!',
+	db: db
 }
